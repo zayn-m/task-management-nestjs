4b0dbd38d5ea0775499674807083ac66b1f60a8a
zayn-m <zain.hassan1212@gmail.com>
2021-06-07 18:53:52 +0500

    pipe validations for filter endpoint

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -3,6 +3,7 @@ import { IsNotEmpty } from 'class-validator';
 export class CreateTaskDTO {
   @IsNotEmpty()
   title: string;
+  
   @IsNotEmpty()
   description: string;
 }
--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { IsIn, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDTO {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
